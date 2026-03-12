dac23770c412c95e3b161fa294ed76d7821f9aaa
robertidell <robertidell@example.com>
2026-03-12 13:02:28 -0400

    Set Puma default port to 3002 for production

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests.
+# Default to 3002 for production (nginx expects this port)
+port ENV.fetch("PORT", 3002)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
